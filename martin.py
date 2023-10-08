3e2dd1c0d28525bfcecc29a2a2063fd42c87efc3
Martin Dengis <103104881+martinDengis@users.noreply.github.com>
2023-10-08 18:34:24 +0200

    attempt a*

--- martin.py
+++ martin.py
@@ -1,8 +1,59 @@
 from pacman_module.game import Agent, Directions
+from pacman_module.util import PriorityQueue
 
+def key(state):
+    """Returns a key that uniquely identifies a Pacman game state.
+
+    Arguments:
+        state: a game state. See API or class `pacman.GameState`.
+
+    Returns:
+        A hashable key tuple.
+    """
+
+    return (
+        state.getPacmanPosition(),
+        tuple(state.getGhostPositions()),
+        tuple(state.getCapsules()),
+        state.getFood(),
+    )
+
+def manhattan_distance(point_a, point_b):
+    """Returns the Manhattan distance between two points.
+    
+    Arguments:
+        point_a: a tuple (x, y) indicating the start position.
+        point_b: a tuple (x, y) indicating the goal position.
+        
+    Returns:
+        The Manhattan distance between point A to point B.
+    """
+    return abs(point_a[0] - point_b[0]) + abs(point_a[1] - point_b[1])
+
+def heuristic(state):
+    """Returns h_cost (heuristic cost) to the closest food dot.
+
+    Arguments:
+        state: a game state. See API or class `pacman.GameState`.
+
+    Returns:
+        The lowest heuristic cost from PacMan to all remaining food dots.
+    """
+
+    min_h_cost = float('inf')
+    pacman_position = state.getPacmanPosition()
+
+    # Transform grid into list of coordinates (x, y) for True values
+    goal_list = state.getFood().asList()
+    for goal_position in goal_list:
+        h_cost = manhattan_distance(pacman_position, goal_position)
+        if h_cost < min_h_cost:
+            min_h_cost = h_cost
+
+    return min_h_cost
 
 class PacmanAgent(Agent):
-    """Pacman agent absed on A* algorithm."""
+    """Pacman agent based on A star (A*) algorithm."""
 
     def __init__(self):
         super().__init__()
@@ -26,66 +77,50 @@ class PacmanAgent(Agent):
             return self.moves.pop(0)
         else:
             return Directions.STOP
-        
+    
     def a_star(self, state):
-        """Given a Pacman game state, returns a list of legal moves to solve
-        the search layout.
-        
-        Arguments:
-            state: a game state. See API or class `pacman.GameState`.
-            
-        Returns:
-            A list of legal moves.
-        """
+            """Given a Pacman game state, returns a list of legal moves to solve
+            the search layout.
 
-        path = []
-        open = set()
-        closed = set()
+            Arguments:
+                state: a game state. See API or class `pacman.GameState`.
 
-        while True:
-            current = open.pop(lowest f_cost)
-            closed.add(current)
+            Returns:
+                A list of legal moves.
+            """
 
-            if current.isWin():
-                return path
-            
-            neighbours = current.getNeighbours()
-            for neighbour in neighbours:
-                if neighbour in closed:
+            path = []
+            fringe = PriorityQueue()
+            fringe.push((state, path), heuristic(state))
+            closed = set()
+
+            while True:
+                if fringe.isEmpty():
+                    return []
+
+                priority, item = fringe.pop()
+                print(priority)
+                current, path = item
+
+
+                if current.isWin():
+                    return path
+
+                current_key = key(current)
+
+                if current_key in closed:
                     continue
+                else:
+                    closed.add(current_key)
 
-                # Check if neighbour is not traversable (get Grid and check boolean at position (x,y) for neighbour)
-                if Grid.neighbour == 1:
-                    continue
-
-                if (new path to neighbour is shorter) or (neighbour is not in open):
-                    neighbour.f_cost = newCost(to determine)
-                    neighbour.parent = current
-                    if neighbour is not in open:
-                        open.add(neighbour)
+                for successor, action in current.generatePacmanSuccessors():
+                    g_cost = len(path) + 1
                     
+                    # Evaluation function f(n) = g(n) + h(n)
+                    f_cost = g_cost + heuristic(state)
 
-""" Pseudocode for A* """
-# OPEN //the set of nodes to be evaluated
-# CLOSED //the set of nodes already evaluated
-# add the start node to OPEN
+                    # Pushing into priority queue a tuple (state, path) and the f_cost
+                    fringe.push((successor, path + [action]), f_cost)
 
-# loop
-#     current = node in OPEN with the lowest f_cost
-#     remove current from OPEN
-#     add current to CLOSED
+            return path
 
-#     if current is the target node //path has been found
-#         return
-
-#     foreach neighbour of the current node
-#         if neighbour is not traversable or neighbour is in CLOSED
-#             skip to the next neighbour
-
-#         if new path to neighbour is shorter OR neighbour is not in OPEN
-#             set f_cost of neighbour
-#             set parent of neighbour to current
-#             if neighbour is not in OPEN
-#                 add neighbour to OPEN
-
-# https://saturncloud.io/blog/implementing-the-a-algorithm-in-python-a-stepbystep-guide/#:~:text=Implementing%20the%20A%2A%20Algorithm%20in%20Python%201%20Node,itself.%20...%204%20Putting%20It%20All%20Together%20
